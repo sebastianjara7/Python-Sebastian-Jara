1784ad9999091b8bba4ef5599c21aff4dcc6fafe
Jeremy Sebastian Apuparo Jara <147017392+sebastianjara7@users.noreply.github.com>
2023-12-12 09:32:43 -0500

    Add files via upload

---
+++ proyecto7.py
@@ -0,0 +1,49 @@
+# El proposito general de este trabajo es presentar lo aprendido en clases, ademas nos ayudo a entender mas a profundidad sobre la programacion aplicando conceptos básicos de Python
+
+# 1 Crea un programa en Python que realice las siguientes operaciones
+
+a = 12 #suma dos numeros int
+b = 7
+c = (a+b)
+print(c)
+d = 74.20 #resta de float
+e = 4.12
+f = (d-e)
+print(f)
+g = 18 #multiplicacion int
+h = 2
+i = (g*h)
+print(i)
+j = 12.30 #division float
+k = 6.30
+l = (j/k)
+print(l)
+#El propósito principal de print es imprimir texto o valores en la pantalla para que puedan ser visualizados por el usuario
+
+#2 Manipulación de Cadenas de Texto
+
+nombre_completo = "Jeremy Sebastian Apuparo Jara" #variable de tipo string que contenga tu nombre completo
+len(nombre_completo)
+print(len(nombre_completo))
+print((nombre_completo),len(nombre_completo))
+#Este programa tiene como finalidad imprimir texto y a su vez es útil cuando necesitas conocer el tamaño de una estructura de datos o la cantidad de elementos en una secuencia
+
+#3 Conversión de Tipos de Datos
+
+# paso 1 Declara una variable entera y otra de punto flotante
+m = 24
+n = 12.50
+print(m) #Realiza la conversión de la variable entera a punto flotante y viceversa
+print(float(m))
+print(n)
+print(int(n))
+print(type(m)) #Imprime en pantalla el tipo de dato original y el tipo de dato después de la conversión
+print(type(n))
+
+#4 Comentarios y Documentación
+
+# Ejercicio 1 se realizo un programa para realizar lo que es operaciones basicas tanto int y float
+# Ejercicio 2 se realizo un programa para conocer el tamaño de una estructura de datos o la cantidad de elementos en una secuencia gracias al uso de len
+# Ejercicio 3 se realizo conversión de la variable entera a punto flotante y viceversa. A su podemos ver el tipo de clase que es como por ejemplo: int o float
+
+
